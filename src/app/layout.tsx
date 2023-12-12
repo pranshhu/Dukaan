ad2235da5c834ad281cf1ff1cde3e5426b87e9d8
Pranshu Gupta <pranshu11111@gmail.com>
2023-12-12 20:50:51 +0530

    v0.0.1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
+import NavBar from '@/components/ui/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +18,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='h-full'>
-      <body className={cn("relative h-full font-sans antialiased", inter.className)}>{children}</body>
+      <body className={cn("relative h-full font-sans antialiased", inter.className)}>
+        <main className='relative flex flex-col min-h-screen'>
+          <NavBar />
+          <div className='flex-grow flex-1'>
+          {children}
+          </div>
+        </main>
+        </body>
     </html>
   )
 }
---
+++ src/components/ui/NavItems.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+
+const NavItems = () => {
+    return (
+        
+    )
+}
+
+export default NavItems
