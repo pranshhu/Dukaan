30c082b683e524cddd57c847946e229a7913eec5
Pranshu Gupta <pranshu11111@gmail.com>
2023-12-16 00:17:54 +0530

    nothing

---
+++ src/config/index.ts
@@ -0,0 +1,12 @@
+export const PRODUCT_CATEGORIES = [
+    {
+        label : "UI kits",
+        value : "ui_kits" as const,
+        featured : [{
+            name : "Editor picks",
+            href: "#",
+            imageSrc: "/nav/ui-kits/mixed.jpg",
+        }]
+
+    }
+]
