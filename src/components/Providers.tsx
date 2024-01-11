d9cfab4338df533440940614d35bee5bdff8eae7
Pranshu Gupta <pranshu11111@gmail.com>
2024-01-11 13:41:07 +0530

    .

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -5,6 +5,7 @@ import { QueryClient } from "@tanstack/react-query"
 
 const Providers = () => {
     const [queryClient] = useState(() => new QueryClient())
+    const [trpcClient] = useState(() => trpc.createClient())
 }
 
 export default Providers
