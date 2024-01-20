0776f98db80e9ac34cde4ab413d6d9e0e7c6a755
Pranshu Gupta <pranshu11111@gmail.com>
2024-01-20 08:11:37 +0530

    sign-in

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { getPayloadClient } from "./get-payload"
 import { nextApp, nextHandler } from "./next-utils"
 import * as trpcExpress from "@trpc/server/adapters/express"
 import { appRouter } from "./trpc"
+import { inferAsyncReturnType } from "@trpc/server"
 
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
@@ -11,6 +12,8 @@ const createContext = ({ req, res }: trpcExpress.CreateExpressContextOptions) =>
     req,
     res,
 })
+
+export type ExpressContext = inferAsyncReturnType<typeof createContext>
 
 const start = async () => {
     const payload = await getPayloadClient({
@@ -24,7 +27,7 @@ const start = async () => {
 
     app.use('/api/trpc', trpcExpress.createExpressMiddleware({
         router: appRouter,
-        createContext
+        createContext,
     }))
 
     app.use((req, res) => nextHandler(req, res))
--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -3,6 +3,7 @@ import { getPayloadClient } from "../get-payload";
 import { AuthCredentialsValidator } from "../lib/validators/account-credentials-validator";
 import { publicProcedure, router } from "./trpc";
 import { z } from "zod";
+import payload from "payload";
 
 export const authRouter = router({
     createPayloadUser: publicProcedure.input(AuthCredentialsValidator).mutation(async ({ input }) => {
@@ -45,5 +46,27 @@ export const authRouter = router({
         if (!isVerified) throw new TRPCError({ code: "UNAUTHORIZED" })
 
         return { sucess: true }
+    }),
+
+    signIn: publicProcedure.input(AuthCredentialsValidator).mutation(async ({ input, ctx }) => {
+        const { email, password } = input
+        const { res } = ctx
+
+        const payload = await getPayloadClient()
+
+        try {
+            await payload.login({
+                collection: "users",
+                data: {
+                    email,
+                    password,
+                },
+                res,
+            })
+
+            return { success: true }
+        } catch (err) {
+            throw new TRPCError({ code: 'UNAUTHORIZED' })
+        }
     }),
 })
