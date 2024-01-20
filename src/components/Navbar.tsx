1da44a6374be40a04ebb5a74b8dae7bcce94f0f1
Pranshu Gupta <pranshu11111@gmail.com>
2024-01-20 15:49:33 +0530

    .

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,10 +4,14 @@ import MaxWidthWrapper from "./MaxWidthWrapper"
 import NavItems from "./NavItems"
 import { buttonVariants } from "./ui/button"
 import Cart from "./Cart"
+import { cookies } from 'next/headers'
+import { getServerSideUser } from '@/lib/payload-utils'
+import UserAccountNav from "./UserAccountNav"
 
-const NavBar = () => {
+const NavBar = async () => {
 
-    const user = null
+    const nextCookies = cookies()
+    const { user } = await getServerSideUser(nextCookies)
 
     return (
         <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
@@ -29,7 +33,7 @@ const NavBar = () => {
                                 <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
                                     { user ? null : ( <Link href='/sign-in' className={buttonVariants({variant:"ghost"})} >Sign in</Link>) }
                                     { user ? null : ( <span className="h-6 w-px bg-gray-200 aria-hidden:'true' " />) }
-                                    { user ? <p></p> : ( <Link href='/sign-up' className={buttonVariants({variant: "ghost"})} >Create account</Link>) }
+                                    { user ? <UserAccountNav /> : ( <Link href='/sign-up' className={buttonVariants({variant: "ghost"})} >Create account</Link>) }
                                     { user ? ( <span className="h-6 w-px bg-gray-200 aria-hidden:'true' " />) : null}
                                     { user ? null : ( <div  className="flex lg:ml-6 "><span className="h-6 w-px bg-gray-200 aria-hidden:'true' " /></div> ) }
 
